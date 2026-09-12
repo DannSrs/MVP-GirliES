a48dfd146cda72b7243cb7faed171ee08e7287e4
Caio <caioxlw@gmail.com>
2026-09-12 02:07:01 -0300

    Deu b.o no commit

--- src/models/Atividade.ts
+++ src/models/Atividade.ts
@@ -1,6 +1,6 @@
 export abstract class Atividade {
   constructor(
-    readonly id: string,
+    private id: string,
     //public tagCategoria: string, // ex: "LAB PRÁTICO - SEMANA 02"
     public titulo: string,
     public tipo: 'AULA' | 'POST' | 'EVENTO'
