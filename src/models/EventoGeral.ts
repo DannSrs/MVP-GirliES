3704c6b1a9dd57772a361b7b697193e02dfbd067
Caioxlw <caioxwl@gmail.com>
2026-09-13 04:06:51 -0300

    modificação nos models para o uso de DTOs

--- src/models/EventoGeral.ts
+++ src/models/EventoGeral.ts
@@ -1,27 +1,8 @@
 import { Atividade } from "./Atividade";
 import { ChecklistItem } from "./ChecklistItem";
 
-export class EventoGeral extends Atividade {
-  public logisticsChecklist: ChecklistItem[] = [];
-
-  constructor(
-    id: string,
-    // tagCategoria: string,
-    titulo: string,
-    public durationInfo: string,
-    public location: string
-  ) {
-    super(id, /*tagCategoria,*/ titulo, 'EVENTO');
-  }
-
-  addLogisticsItem(item: ChecklistItem): void {
-    this.logisticsChecklist.push(item);
-  }
-
-  getLogisticsProgress(): number {
-    if (this.logisticsChecklist.length === 0) return 0;
-    
-    const completed = this.logisticsChecklist.filter(item => item.isCompleted).length;
-    return (completed / this.logisticsChecklist.length) * 100;
-  }
+export interface EventoGeral extends Atividade {
+  durationInfo: string;
+  location: string;
+  logisticsChecklist?: ChecklistItem[];
 }
